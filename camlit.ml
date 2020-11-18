0f0d41ce94fe4eb9947989d3c41c5e38a0c99da7
Richard Bonichon <richard.bonichon@tweag.io>
2020-11-18 10:18:20 +0100

    Basic init subcommand
    https://www.leshenko.net/p/ugit/#cli-argparse

--- camlit.ml
+++ camlit.ml
@@ -1,1 +1,21 @@
-let () = print_endline "Hello, World!"
+let args = ref [ ("-init", Arg.Unit Cmds.init, "") ]
+
+let subcommands = [ ("init", Arg.Unit Cmds.init) ]
+
+let umsg = "Bad usage. Refer to documentation."
+
+type parsing_state = Subcommand | File
+
+let () =
+  let parsing_state = ref Subcommand in
+  let files = ref [] in
+  Arg.parse_dynamic args
+    (fun str ->
+      match !parsing_state with
+      | File -> files := str :: !files
+      | Subcommand ->
+          args :=
+            [ ("-foo", Arg.Unit (fun () -> print_endline "foo"), " Test") ];
+          parsing_state := File)
+    umsg;
+  Format.printf "Ignore %d file names" (List.length !files)
---
+++ cmds.ml
@@ -0,0 +1,11 @@
+let default_directory = ".camlit"
+
+let create_directory ?(warn = false) directory_name =
+  if warn && Sys.file_exists directory_name then
+    Format.eprintf "A directory or file named %s already exists@."
+      directory_name;
+  Unix.mkdir directory_name 0o700
+
+let init () =
+  Format.printf "Initializing directory %s@." default_directory;
+  create_directory default_directory
