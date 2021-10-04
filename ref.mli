a7a416274cc36f5e466b77cdf2cab18374db9aae
Richard Bonichon <richard.bonichon@gmail.com>
2021-10-04 21:42:12 +0200

    typo fix

--- ref.mli
+++ ref.mli
@@ -4,6 +4,6 @@ val pp : Format.formatter -> t -> unit
 
 val of_string : string -> t
 (** [of_string s] takes a string value [s] and "does the right thing", that is:
- - if the string is a valide hex identifiier, it defines an oid;
+ - if the string is a valid hex identifiier, it defines an oid;
  - other it assumes it is a ref 
 *)
